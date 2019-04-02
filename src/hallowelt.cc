c72fc0898b9d7410ed396cb573dba7d7ed899d30
sebastianbierb <sebastian-bierbass@gmx.de>
2019-04-02 16:02:32 +0200

    change cc file

--- src/hallowelt.cc
+++ src/hallowelt.cc
@@ -1,6 +1,11 @@
 #include<iostream>
+#include<string>
+using namespace std;
 
-int main(){
-    std::cout << "Hallo Welt!" << std::endl;
+int main(int argc, char** argv){
+    enum languages {german, english, french};
+    std::string greetings[3] = {"Hallo Welt!", "Hello world!", "Salut monde!"};
+    languages lang = french;
+    std::cout << greetings[lang] << std::endl;
     return 0;
 }
