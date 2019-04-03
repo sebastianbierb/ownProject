462941767469c99ffc5dd0e0daf5bf7a22abc998
sebastianbierb <sebastian-bierbass@gmx.de>
2019-04-03 16:35:24 +0200

    commit master hello

--- src/hallowelt.cc
+++ src/hallowelt.cc
@@ -1,11 +1,10 @@
 #include<iostream>
 #include<string>
-<<<<<<< HEAD
 using namespace std;
 
 int main(int argc, char** argv){
-    enum languages {german, english, french};
-    string greetings[3] = {"Hallo Welt!", "Hello world!", "Salut monde!"};
+    enum languages {german, english, french, spanish};
+    string greetings[4] = {"Hallo Welt!", "Hello world!", "Salut monde!", "Ich kein spanisch kann"};
     languages lang = french;
     cout << greetings[lang] << endl;
     return 0;
